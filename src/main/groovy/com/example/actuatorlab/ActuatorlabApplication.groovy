6c912d0e819839173f74af7b421a67cacbb5d16a
leadsoftengrlalusin <johnrovic.lalusin@gcash.com>
2025-08-04 17:38:41 +0800

    git init

---
+++ src/main/groovy/com/example/actuatorlab/ActuatorlabApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.actuatorlab
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ActuatorlabApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ActuatorlabApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/example/actuatorlab/ActuatorlabApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.actuatorlab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActuatorlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
